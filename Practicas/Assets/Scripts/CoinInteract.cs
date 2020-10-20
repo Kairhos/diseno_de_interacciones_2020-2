802d76e81895476f984cb281c7dd8005aeb60fc6
Miguel <gilberto.maldonado.miguel@uabc.edu.mx>
2020-10-20 13:55:17 -0700

    Pract03

---
+++ Practicas/Assets/Scripts/CoinInteract.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinInteract : MonoBehaviour {
+
+    public Rigidbody rb;
+    bool isInsideZone = false;
+    public float torque;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isInsideZone){
+            if(Input.GetKeyDown(KeyCode.Alpha1))
+                transform.GetComponent<Renderer>().material.SetColor("_Color",Color.blue);
+            else if(Input.GetKeyDown(KeyCode.Alpha2))
+                transform.GetComponent<Renderer>().material.SetColor("_Color",Color.red);
+        }
+        
+    }
+
+    private void FixedUpdate() {
+        if(isInsideZone && Input.GetKeyDown(KeyCode.I))
+            rb.AddTorque(transform.up * torque * -1f);
+    }
+
+    void Awake() {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    public virtual void Interact(){
+        Debug.Log("Ejecutando Interaccion...");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+            return;
+        isInsideZone = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+            return;
+        isInsideZone = false;
+    }
+}
---
+++ Practicas/Assets/Scripts/Interactable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    bool isInsideZone = false;
+
+    public virtual void Interact()
+    {
+        Debug.Log("Ejecutando interaccion");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isInsideZone && Input.GetKeyDown(KeyCode.I))
+        {
+            Interact();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+            return;
+
+        //Debug.Log("Traspaso la esfera: " + other.gameObject.name);
+        isInsideZone = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+            return;
+
+        //Debug.Log("Traspaso la esfera: " + other.gameObject.name);
+        isInsideZone = false;
+    }
+}
