0069c91899155bd306d4f9cc6da86cc852c8df15
Miguel <gilberto.maldonado.miguel@uabc.edu.mx>
2020-12-14 01:08:48 -0800

    Pract06
    Interacciones de Realidad Virtual por medio de Google VR SDK (Cardboard)

---
+++ Practicas_Unity_2019/Assets/Locomotion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locomotion : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 heightOffset;
+    // Start is called before the first frame update
+    public void TeleportPlayer(Vector3 newPosition)
+    {
+        player.position = newPosition + heightOffset;
+    }
+}
